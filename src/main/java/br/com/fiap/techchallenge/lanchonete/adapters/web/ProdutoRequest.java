1c68a8665f9d35aa16e8af7e4eb78d44e3ac82e3
Pedro Pereira <pedrojr.19@hotmail.com>
2023-06-18 12:37:07 -0300

    fix() corrige validação dos campos de ProdutoRequest

--- src/main/java/br/com/fiap/techchallenge/lanchonete/adapters/web/ProdutoRequest.java
+++ src/main/java/br/com/fiap/techchallenge/lanchonete/adapters/web/ProdutoRequest.java
@@ -2,26 +2,34 @@ package br.com.fiap.techchallenge.lanchonete.adapters.web;
 
 import br.com.fiap.techchallenge.lanchonete.core.domain.models.Categoria;
 import br.com.fiap.techchallenge.lanchonete.core.domain.models.ProdutoIn;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record ProdutoRequest(String nome, Categoria categoria, BigDecimal preco, String descricao) implements ProdutoIn {
 
+    @NotBlank(message = "O campo 'nome' é obrigatório")
     @Override
     public String getNome() {
         return nome;
     }
 
+    @NotNull(message = "O campo 'categoria' é obrigatório")
     @Override
     public Categoria getCategoria() {
         return categoria;
     }
 
+    @NotNull(message = "O campo 'preco' é obrigatório")
+    @DecimalMin(value = "0.0", message = "Informe um valor maior que 0.0")
     @Override
     public BigDecimal getPreco() {
         return preco;
     }
 
+    @NotBlank(message = "O campo 'descricao' é obrigatório")
     @Override
     public String getDescricao() {
         return descricao;
