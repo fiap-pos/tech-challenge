f0472826c929ae782c4be587759c2e3c66c75ba4
Marcos O. Alves <eng.marcos.alves@gmail.com>
2024-01-16 22:43:36 -0300

    implementando testes - mapper Cobranca e ItemPedido, ajuste no CobrancaHelper

--- src/test/java/br/com/fiap/techchallenge/lanchonete/adapters/repository/mappers/CobrancaMapperTest.java
+++ src/test/java/br/com/fiap/techchallenge/lanchonete/adapters/repository/mappers/CobrancaMapperTest.java
@@ -1,21 +1,45 @@
 package br.com.fiap.techchallenge.lanchonete.adapters.repository.mappers;
 
+import br.com.fiap.techchallenge.lanchonete.adapters.repository.models.Cobranca;
+import br.com.fiap.techchallenge.lanchonete.adapters.repository.models.Produto;
+import br.com.fiap.techchallenge.lanchonete.core.dtos.CobrancaDTO;
+import br.com.fiap.techchallenge.lanchonete.utils.adapters.web.CobrancaHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static br.com.fiap.techchallenge.lanchonete.utils.adapters.web.CobrancaHelper.getCobranca;
+import static br.com.fiap.techchallenge.lanchonete.utils.adapters.web.CobrancaHelper.getCobrancaDTO;
+import static br.com.fiap.techchallenge.lanchonete.utils.adapters.web.ProdutoHelper.getProdutoDTO;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CobrancaMapperTest {
 
+    private CobrancaMapper cobrancaMapper;
+
     @BeforeEach
     void setUp() {
+        this.cobrancaMapper = new CobrancaMapper();
     }
 
     @Test
-    void toCobranca() {
+    void dadoProdutoDTO_DeveFazerMapper_RetornarProduto() {
+        var cobrancaDTO = getCobrancaDTO();
+
+        var cobranca = cobrancaMapper.toCobranca(cobrancaDTO);
+
+        assertThat(cobranca).isNotNull().isInstanceOf(Cobranca.class);
+        assertThat(cobranca.getPedidoId()).isEqualTo(cobrancaDTO.pedidoId());
     }
 
     @Test
-    void toCobrancaOut() {
+    void dadoCobranca_DeveFazerMapper_RetornarCobrancaDTO() {
+        var cobranca = getCobranca();
+
+        var cobrancaDTO = cobrancaMapper.toCobrancaOut(cobranca);
+
+        assertThat(cobrancaDTO).isNotNull().isInstanceOf(CobrancaDTO.class);
+        assertThat(cobrancaDTO.id()).isEqualTo(cobranca.getId());
+        assertThat(cobrancaDTO.pedidoId()).isEqualTo(cobranca.getPedidoId());
     }
 }
--- src/test/java/br/com/fiap/techchallenge/lanchonete/utils/adapters/web/CobrancaHelper.java
+++ src/test/java/br/com/fiap/techchallenge/lanchonete/utils/adapters/web/CobrancaHelper.java
@@ -1,10 +1,12 @@
 package br.com.fiap.techchallenge.lanchonete.utils.adapters.web;
 
+import br.com.fiap.techchallenge.lanchonete.adapters.repository.models.Cobranca;
 import br.com.fiap.techchallenge.lanchonete.core.domain.entities.QrCode;
 import br.com.fiap.techchallenge.lanchonete.core.domain.entities.enums.StatusCobrancaEnum;
 import br.com.fiap.techchallenge.lanchonete.core.dtos.CobrancaDTO;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public abstract class CobrancaHelper {
 
@@ -14,7 +16,14 @@ public abstract class CobrancaHelper {
     private static final StatusCobrancaEnum STATUS = StatusCobrancaEnum.PAGO;
     private static final QrCode QR_CODE = new QrCode("e3BlZGlkb0lkOjEsdmFsb3I6NDUuOX0=");
 
+    private static final LocalDateTime CREATED_AT = LocalDateTime.MAX;
+    private static final LocalDateTime UPDATED_AT = LocalDateTime.MAX;
+
     public static CobrancaDTO getCobrancaDTO() {
         return new CobrancaDTO(ID, PEDIDO_ID, VALOR, STATUS, QR_CODE);
+    }
+
+    public static Cobranca getCobranca() {
+        return new Cobranca(ID, PEDIDO_ID, STATUS, VALOR, QR_CODE.getDecodedBase64Value(), CREATED_AT, UPDATED_AT);
     }
 }
