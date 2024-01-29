3e5fe6aef6518d03fbc3a15286a9db59bc2b2169
Marcos O. Alves <eng.marcos.alves@gmail.com>
2024-01-28 23:56:26 -0300

    cobertura 83%

--- src/test/java/br/com/fiap/techchallenge/lanchonete/adapters/messages/listeners/PagamentosListenerTest.java
+++ src/test/java/br/com/fiap/techchallenge/lanchonete/adapters/messages/listeners/PagamentosListenerTest.java
@@ -1,10 +1,8 @@
 package br.com.fiap.techchallenge.lanchonete.adapters.messages.listeners;
 
-import br.com.fiap.techchallenge.lanchonete.adapters.repository.ClienteRepository;
 import br.com.fiap.techchallenge.lanchonete.core.ports.in.pedido.AtualizaStatusPedidoInputPort;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.instancio.Instancio;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,11 +10,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import software.amazon.awssdk.services.sqs.model.Message;
 
-import javax.management.InstanceAlreadyExistsException;
-
 import static br.com.fiap.techchallenge.lanchonete.utils.CobrancaHelper.getCobrancaDTO;
 import static br.com.fiap.techchallenge.lanchonete.utils.PedidoHelper.getPedidoDTO;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
